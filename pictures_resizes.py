f79fd55da0c350fdf9e12ab442f211f71518c31d
Chris5908 <christophe.pere0313@orange.fr>
2017-02-16 08:12:32 +0100

    Update pictures_resizes.py
    Increase performance and automate choice of the final pictures
    Test if the picture is higher than a minimal size in pixel and return a picture modified and a variable wich count the number of files modified

--- pictures_resizes.py
+++ pictures_resizes.py
@@ -3,20 +3,30 @@ from scipy import misc                                                          
 from skimage.io import imread
 import numpy as np
 import glob
+from os.path import join
 
-pictures_list = glob.glob("D:/Users/chpere/Documents/Python/Projects_Python/*jpg")                          # permit to save all the files in the path with the extension selected
+path = "D:/Users/chpere/Documents/Python/Projects_Python/Data_set" 
+pictures_list = [] # [glob.glob(path) for path in ["*.png","*.jpeg","*.jpg"]]                          # permit to save all the files in the path with the extension selected
+for ext in ('*.gif', '*.png', '*.jpg', '*.jpeg','*.bmp'):
+   pictures_list.extend(glob.glob(join(path, ext)))
 print("You have {} in your files, process to change resolution in progress".format(len(pictures_list)))     # print the number of files in the folder
 
+crit = 150                                                                      # minimum size of height or length of the picture
+img_modif = 0
 for files in pictures_list:                                                     # loop for in the number of files
     img = open(files,'r')                                                       # open the pictures 
     im = imread(files)                                                          # and read it
-
-    img2 = misc.imresize(im, 25, interp='bicubic')                              # resize the picture 25% of the original 
-    misc.imsave(files[:len(files)-4]+'2.jpg', img2)                             # save the pictures with the name (path included) with the number 2
+    #print(len(im), len(im[0]))
+    #size = (1/(min(len(im),len(im[0]))/100))*100
+    if(min(len(im),len(im[0]))>crit):                                           # enter the loop if the picture is larger ou higher of the criteria crit (number minimal of pixels)
+        img2 = misc.imresize(im, int((1/(min(len(im),len(im[0]))/crit))*100), interp='bicubic') # resize the picture 25% of the original 
+        misc.imsave(files[:len(files)-4]+'2.jpg', img2)                                         # save the pictures with the name (path included) with the number 2
+    #print(size)
+        new_img = files.split("\\")                                                             # split the path to access to the name of the files
     
-    new_img = files.split("\\")                                                 # split the path to access to the name of the files
+        print("The picture {} was created".format(new_img[len(new_img)-1]))                     # avert the user that the pictures was changed 
+        img_modif += 1                                                                          # number of loop / pictures modified 
     
-    print("The picture {} was changed".format(new_img[len(new_img)-1]))         # avert the user that the pictures was changed 
+    img.close()
     
-    
-    
+print("\nYou have modified {} pictures".format(img_modif))                                      # say how many pictures was mdified 
